6b6396bcd680f1ad5b072c78ba9d0328f88d5885
ISMAEL FRANCISCO MORENO <ismaelfcom93@gmail.com>
2024-09-22 14:08:12 -0500

    test env var

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ const Home = () => {
         Promptopia is an open-source AI prompting tool for modern world to
         discover, create and share creative prompts
       </p>
+      <p>{process.env.ENV_VAR}</p>
       <Feed />
     </section>
   );
