34cf75233654b46edcb3149d473474d48e93a852
Ismael <ismaelfcom93@gmail.com>
2025-02-18 15:53:56 -0600

    fix: add optional chaining for prompt creator properties

--- components/PromptCard.tsx
+++ components/PromptCard.tsx
@@ -35,7 +35,7 @@ const PromptCard = ({
       <div className="flex justify-between items-start gap-5">
         <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
           <Image
-            src={prompt.creator.image}
+            src={prompt.creator?.image}
             alt="user_image"
             width={40}
             height={40}
@@ -43,10 +43,10 @@ const PromptCard = ({
           />
           <div className="flex flex-col">
             <h3 className="font-satoshi font-semibold text-gray-900">
-              {prompt.creator.username}
+              {prompt.creator?.username}
             </h3>
             <p className="font-inter text-sm text-gray-500">
-              {prompt.creator.email}
+              {prompt.creator?.email}
             </p>
           </div>
         </div>
@@ -71,7 +71,7 @@ const PromptCard = ({
       >
         {prompt.tag}
       </p>
-      {session?.user.id === prompt.creator._id.toString() &&
+      {session?.user.id === prompt.creator?._id.toString() &&
         pathname === '/profile' && (
           <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
             <p
